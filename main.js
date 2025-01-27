dfecefc1597666cc1fa04551581499de8027c559
ppanda48 <undogableplays@gmail.com>
2025-01-27 16:18:19 +0630

    Fix 1.1.1

--- main.js
+++ main.js
@@ -168,7 +168,7 @@ let poweronhours = [
 
     {timeon:[13,25],date:[20,23,26,29],month:"0",group:"C"},
     {timeon:[9,21],date:[21,24,27,30],month:"0",group:"C"},
-    {timeon:[5,17,25],date:[22,25,28,31],month:"0",group:"C"}
+    {timeon:[5,17,25],date:[22,25,28,31],month:"0",group:"C"},
 
     {timeon:[5,17,25],date:[1,4,7,10,13,16,19,22,25,28],month:"1",group:"A"},
     {timeon:[13,25],date:[2,5,8,11,14,17,20,23,26],month:"1",group:"A"},
@@ -180,7 +180,7 @@ let poweronhours = [
 
     {timeon:[13,25],date:[1,4,7,10,13,16,19,22,25,28],month:"1",group:"C"},
     {timeon:[9,21],date:[2,5,8,11,14,17,20,23,26],month:"1",group:"C"},
-    {timeon:[5,17,25],date:[3,6,9,12,15,18,21,24,27],month:"1",group:"C"}
+    {timeon:[5,17,25],date:[3,6,9,12,15,18,21,24,27],month:"1",group:"C"},
 ];
 
 var todayPowerOnHours;
